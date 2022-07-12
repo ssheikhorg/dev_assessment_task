e3a46f666ce33addcab32c5d848a802fce83b254
Shapon Sheikh <shsheikhbd@gmail.com>
2022-07-13 00:17:57 +0600

    created webhook

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,14 +1,27 @@
 from flask import Blueprint, json, request
+from ..extensions import insert_extension
+from datetime import datetime
 
-webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
-@webhook.route('/receiver', methods=["POST"])
+webhook = Blueprint("Webhook", __name__, url_prefix="/webhook")
+
+
+@webhook.route("/receiver", methods=["POST"])
 def receiver():
-    # github webhook
-    data = request.get_json()
-    return {"Hello": "world"}, 200
+    if request.headers["Content-Type"] == "application/json":
+        event = request.json
+        print(event)
+        if request.headers["X-GitHub-Event"] == "push":
+            data = {
+                "request_id": event["request_id"],
+                "author": event["sender"]["login"],
+                "action": event["action"],
+                "from_branch": event["repository"]["name"],
+                "to_branch": event["repository"]["full_name"],
+                "timestamp": datetime.timestamp(datetime.now()),
+            }
 
 
-@webhook.route('/', methods=["GET"])
+@webhook.route("/", methods=["GET"])
 def index():
     return json.dumps({"Hello": "world"}), 200
